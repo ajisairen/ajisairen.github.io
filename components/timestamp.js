dc90556dd9fd8e791e0e16b30a97f3fbfee32c8d
Serena Xin <xin00051@umn.edu>
2024-11-17 16:35:00 -0600

    font size edit

--- components/timestamp.js
+++ components/timestamp.js
@@ -4,6 +4,7 @@ import { useColorMode } from '@chakra-ui/react'
 const TimeStamp = styled.text`
   text-align: justify;
   text-indent: 1em;
+  font-size: 0.8em;
   hyphens: auto;
   color: ${({ colorMode }) => (colorMode === 'light' ? '#a0aec0' : '#718096')};
 `
