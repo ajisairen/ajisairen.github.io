6f2f043f791a91e73a702992ddbfee7a8cdfdb85
Serena Xin <116854626+ajisairen@users.noreply.github.com>
2026-07-10 01:04:05 -0500

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -7,20 +7,20 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=devicee-width, initial-scale=1" />
-        <meta name="description" content="Serena's Homepage" />
-        <meta name="author" content="Serena Xin" />
+        <meta name="description" content="Homepage" />
+        <meta name="author" content="ajisairen" />
         <meta name="author" content="ajisairen" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta property="og:url" content="https://ajisairen.github.io/" />
         <meta name="theme-color" content="#9576BC" data-react-helmet="true" />
-        <meta name="og:title" content="Serena Xin" />
+        <meta name="og:title" content="ajisairen" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/images/card.png" />
         <meta
           property="og:description"
-          content="Serena's personal portfolio website"
+          content="personal portfolio website"
         />
-        <title>Serena Xin - Homepage</title>
+        <title>Homepage</title>
       </Head>
 
       <Navbar path={router.asPath} />
